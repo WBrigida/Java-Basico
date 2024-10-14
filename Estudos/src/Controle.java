ba707a44fabf7817b13ad6e1c53e6ea9664a059f
WBrigida <wandersonbrigida15@gmail.com>
2024-10-14 18:24:54 -0300

    exercicio de POO

---
+++ Estudos/src/Controle.java
@@ -0,0 +1,22 @@
+public class Controle {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        smartTv.ligar();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual : " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual : " + smartTv.canal);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+    }
+}
---
+++ Estudos/src/SmartTv.java
@@ -0,0 +1,43 @@
+public class SmartTv {
+    Boolean ligada = false;
+    int canal = 5;
+    int volume = 35;
+
+    public void mudarCanal(int novoCnal) {
+        canal = novoCnal;
+        System.out.println("Mudando para o canal " + canal);
+    }
+
+    public void aumentarCanal() {
+        // canal = canal + 1;
+        canal++;
+        System.out.println("Mudando para o canal " + canal);
+    }
+
+    public void diminuirCanal() {
+        // canal = canal - 1;
+        canal--;
+        System.out.println("Mudando para o canal " + canal);
+    }
+
+    public void aumentarVolume() {
+        // volume = volume + 1;
+        volume++;
+        System.out.println("Aumentando o volume para " + volume);
+    }
+
+    public void diminuirVolume() {
+        // volume = volume -1;
+        volume--;
+        System.out.println("Diminuindo o volume para " + volume);
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+}
